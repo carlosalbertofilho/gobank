0adcb648dcef0046f4b8319c65bdcd79e946b323
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-05-16 18:25:39 +0000

    server: define server

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"github.com/carlosalbertofilho/gobank/api"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	server := api.NewAPIServer(":8080")
+	server.Run()
 }
